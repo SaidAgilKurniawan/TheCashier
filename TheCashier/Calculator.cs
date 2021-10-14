03f30b22a7942e343b7491779dd3d6d0fd8ea25e
SaidAgilKurniawan <92496571+SaidAgilKurniawan@users.noreply.github.com>
2021-10-14 10:12:14 +0700

    Add files via upload

---
+++ TheCashier/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TheCashier
+{
+    internal class Calculator
+    {
+        private List<Item> listItem;
+        private double total = 0;
+
+        public Calculator()
+        {
+            this.listItem = new List<Item>();
+        }
+        public void addItem(Item Item)
+        {
+            this.listItem.Add(Item);
+            this.total += Item.getSubTotal();
+        }
+        public double getTotal()
+        {
+            return total;
+        }
+
+        public List<Item> getListItem()
+        {
+            return listItem;
+        }
+    }
+}
